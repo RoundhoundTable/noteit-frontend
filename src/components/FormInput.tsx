7af4a497f1701fcc72616c82977391bf28a306b6
Zero1Exp <reyestobias.037@gmail.com>
2022-06-08 18:32:03 -0300

    refactor: Modify input classes and structure

--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -6,13 +6,13 @@ interface IFormInputProps {
 export const FormInput = ({ label, inputType }: IFormInputProps) => {
   return (
     <>
-      <label className="flex flex-col text-sm font-montserrat lg:px-2 text-left">
+      <label className="flex flex-col text-sm font-montserrat text-left text-primary-700 mt-2">
         {label}
+        <input
+          className="flex flex-col h-8 bg-primary-500/40 rounded-md"
+          type={inputType}
+        />
       </label>
-      <input
-        className="flex flex-col px-3 h-8 bg-primary-500/40 mt-1"
-        type={inputType}
-      />
     </>
   );
 };
