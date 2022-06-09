4bae31f88100f98a13054c68166643dfe3dc010e
Fabricio Futryk <ffutryk52@gmail.com>
2022-06-09 02:37:39 -0300

    feat: Add React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import { Route, Router, Routes } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
 import "./index.css";
-import { Login } from "./pages/LoginPage";
+import { MainLayout } from "./layouts/MainLayout";
+import { LoginPage } from "./pages/LoginPage";
+import { RegisterPage } from "./pages/RegisterPage";
+
+const TEST = () => <h1>ola</h1>
+const TEST2 = () => <h1>ola2</h1>
+
 
 const App = () => {
   return (
     <div className="font-montserrat">
-      <Login />
+      <Routes>        
+        <Route element={<MainLayout/>}>
+          <Route path="/"/>
+        </Route>
+        <Route path="account">
+          <Route path="login" element={<LoginPage/>}/>
+          <Route path="register" element={<RegisterPage/>}/>
+        </Route>
+      </Routes>
     </div>
   );
 };
