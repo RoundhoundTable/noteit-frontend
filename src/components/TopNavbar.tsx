5cd912ff215ee16ad6a7bad06a8bfb79b9815dbc
Ignun <bajunazu@gmail.com>
2022-06-10 20:13:45 -0300

    refactor: Made top navbar fixed

--- src/components/TopNavbar.tsx
+++ src/components/TopNavbar.tsx
@@ -4,7 +4,7 @@ import { Actions } from "./Actions";
 
 export const TopNavbar = () => {
   return (
-    <nav className="flex flex-row justify-around py-2 shadow-xl shadow-primary-500/10 bg-white">
+    <nav className="fixed inset-x-0 flex flex-row justify-around py-2 shadow-xl shadow-primary-500/10 bg-white">
       <Search />
       <Logo className="h-11"/>
       <Actions className="flex-row align-middle hidden md:flex justify-between w-64"/>
