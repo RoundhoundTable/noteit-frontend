c6dc514a75798403b4024096e667418b684c180f
Fabricio Futryk <ffutryk52@gmail.com>
2022-06-10 15:40:24 -0300

    feat: Add profile and error page to router

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@ import { Route, Router, Routes } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
 import "./index.css";
 import { MainLayout } from "./layouts/MainLayout";
+import { ErrorPage } from "./pages/ErrorPage";
 import { LoginPage } from "./pages/LoginPage";
+import { ProfilePage } from "./pages/ProfilePage";
 import { RegisterPage } from "./pages/RegisterPage";
-
-const TEST = () => <h1>ola</h1>
-const TEST2 = () => <h1>ola2</h1>
-
 
 const App = () => {
   return (
@@ -15,6 +13,8 @@ const App = () => {
       <Routes>        
         <Route element={<MainLayout/>}>
           <Route path="/"/>
+          <Route path="/u/:username" element={<ProfilePage/>}/>
+          <Route path="*" element={<ErrorPage/>}/>
         </Route>
         <Route path="account">
           <Route path="login" element={<LoginPage/>}/>
