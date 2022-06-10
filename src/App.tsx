1de14c8a4af6c2da3998119ddac34a431552bf38
Ignun <bajunazu@gmail.com>
2022-06-10 20:09:53 -0300

    feat: Add Main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { Route, Router, Routes } from "react-router-dom";
-import { Navbar } from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
 import "./index.css";
 import { MainLayout } from "./layouts/MainLayout";
 import { ErrorPage } from "./pages/ErrorPage";
 import { LoginPage } from "./pages/LoginPage";
+import { MainPage } from "./pages/MainPage";
 import { ProfilePage } from "./pages/ProfilePage";
 import { RegisterPage } from "./pages/RegisterPage";
 
@@ -12,7 +12,7 @@ const App = () => {
     <div className="font-montserrat">
       <Routes>        
         <Route element={<MainLayout/>}>
-          <Route path="/"/>
+          <Route path="/" element={<MainPage/>}/>
           <Route path="/u/:username" element={<ProfilePage/>}/>
           <Route path="*" element={<ErrorPage/>}/>
         </Route>
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,10 @@
+import { Feed } from "../components/Feed";
+
+export const MainPage = () => {
+    return (
+        <>
+            <h1>Main Page</h1>
+            <Feed />
+        </>
+    );
+}
