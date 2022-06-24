57a025bfc065bfc1cd4b47501cdbe8c7f01a1129
01R4D-M <monterosrubendario@gmail.com>
2022-06-24 18:52:23 -0300

    refactor: add conditional rendering to the search and actions bars

--- src/components/TopNavbar.tsx
+++ src/components/TopNavbar.tsx
@@ -1,13 +1,21 @@
 import { Search } from "./Search";
 import { Logo } from "./Logo";
 import { Actions } from "./Actions";
+import { useEffect, useState } from "react";
 
 export const TopNavbar = () => {
+  const [width, setWidth] = useState<number>(window.innerWidth)
+
+  useEffect(() => {
+      window.addEventListener('resize', () => setWidth(window.innerWidth))
+      return () => window.removeEventListener('resize', () => setWidth(window.innerWidth))
+  });
+  
   return (
     <nav className="sticky w-full flex flex-row justify-around py-2 shadow-xl shadow-primary-500/10 bg-white">
-      <Search />
+      {width >= 768 && <Search />}
       <Logo className="h-11"/>
-      <Actions className="flex-row align-middle hidden md:flex justify-between w-64"/>
+      {width >= 768 && <Actions className="flex-row align-middle hidden md:flex justify-between w-64"/>}
     </nav>
   );
 };
