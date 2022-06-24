ae1722c51ff976b29a6da95514405465dd8a97ec
01R4D-M <monterosrubendario@gmail.com>
2022-06-24 18:54:25 -0300

    refactor: add conditional rendering to the bottom navbar

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,13 +1,21 @@
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { BottomNavbar } from "../components/BottomNavbar";
 import { TopNavbar } from "../components/TopNavbar";
 
 export const MainLayout = () => {
+    const [width, setWidth] = useState<number>(window.innerWidth)
+
+    useEffect(() => {
+        window.addEventListener('resize', () => setWidth(window.innerWidth))
+        return () => window.removeEventListener('resize', () => setWidth(window.innerWidth))
+    });
+
     return(
         <div className="flex flex-col w-full">
             <TopNavbar/>
             <Outlet />
-            <BottomNavbar/>
+            {width <= 767 && <BottomNavbar/>}
         </div>
     );
 }
