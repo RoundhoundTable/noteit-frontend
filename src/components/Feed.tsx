a24a0885017e385c179a07caea7a655ba59186ca
01R4D-M <monterosrubendario@gmail.com>
2022-06-24 18:47:41 -0300

    fix: change margin size to the feed container

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -2,7 +2,7 @@ import { Note } from "./Note";
 
 export const Feed = () => {
     return (
-      <div className="flex flex-col justify-center items-center mt-8 p-8 md:p-0">
+      <div className="flex flex-col justify-center items-center mt-4 mb-16 p-8 md:p-0">
         <Note />
         <Note />
         <Note />
