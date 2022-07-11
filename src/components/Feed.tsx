156c4446eb19fc1057397313e1b89717afb5a629
Ignun <bajunazu@gmail.com>
2022-07-11 19:01:40 -0300

    Refactor: change Note to PartialNote

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,11 +1,11 @@
-import { Note } from "./Note";
+import { PartialNote } from "../components/PartialNote";
 
 export const Feed = () => {
     return (
       <div className="flex flex-col justify-center items-center mt-4 mb-16 p-8 md:p-0">
-        <Note />
-        <Note />
-        <Note />
+        <PartialNote />
+        <PartialNote />
+        <PartialNote />
       </div>
     );
   };
