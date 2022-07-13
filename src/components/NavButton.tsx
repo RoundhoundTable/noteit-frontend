0714ab32ae30fb09eeb20914ad72bafbb4736e9b
01R4D-M <monterosrubendario@gmail.com>
2022-07-13 19:44:37 -0300

    Refactor: add onClick property

--- src/components/NavButton.tsx
+++ src/components/NavButton.tsx
@@ -1,15 +1,17 @@
 import { Icon } from "@iconify/react";
+import { MouseEventHandler } from "react";
 import { Link } from "react-router-dom";
 
 interface INavButtonProp {
   icon: string;
   link?: string;
+  onClick?: MouseEventHandler;
 }
 
-export const NavButon = ({ icon, link = "" }: INavButtonProp) => {
+export const NavButon = ({ icon, link = "", onClick }: INavButtonProp) => {
   return (
     <Link to={link} className="mx-6 py-2">
-      <Icon className="w-7 h-7 text-primary-500" icon={icon} />
+      <Icon className="w-7 h-7 text-primary-500" icon={icon} onClick={onClick} />
     </Link>
   );
 };
