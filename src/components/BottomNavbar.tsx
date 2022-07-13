2c25f59a5d24537646a78986f6d787cb0a1aba68
01R4D-M <monterosrubendario@gmail.com>
2022-07-13 19:36:17 -0300

    Refactor: position to sticky

--- src/components/BottomNavbar.tsx
+++ src/components/BottomNavbar.tsx
@@ -2,8 +2,8 @@ import { Actions } from "./Actions"
 
 export const BottomNavbar = () => {
     return (
-        <div className="fixed bottom-0 w-full bg-white shadow shadow-primary-500">
-            <Actions className="flex flex-row align-middle md:hidden h-12 w-full justify-around"/>
+        <div className="sticky bottom-0 w-full bg-white shadow shadow-primary-500">
+            <Actions className="flex flex-row align-middle w-full justify-around"/>
         </div>
     )
 }
