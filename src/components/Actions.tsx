2bb099e76cd96be80f837cddb970c5b29b44f6d7
01R4D-M <monterosrubendario@gmail.com>
2022-07-13 19:34:30 -0300

    Feat: add dropdown menu

--- src/components/Actions.tsx
+++ src/components/Actions.tsx
@@ -1,16 +1,36 @@
+import { useEffect, useState } from "react";
 import { NavButon } from "./NavButton";
+import { DropdownMenu } from "./DropdownMenu";
 
 interface IActionsProp {
   className?: string
 }
 
 export const Actions = ({className}: IActionsProp) => {
+  const isLoggedIn = true;
+  
+  const [ showDropdown, setShowDropdown ] = useState<boolean>(false)
+
+  const toggleDropdown = () => setShowDropdown (!showDropdown);
+
+  const [width, setWidth] = useState<number>(window.innerWidth)
+
+  useEffect(() => {
+      window.addEventListener('resize', () => setWidth(window.innerWidth))
+      return () => window.removeEventListener('resize', () => setWidth(window.innerWidth))
+  });
+
   return (
-    <div className={className}>
-      <NavButon icon="ant-design:home-filled" />
-      <NavButon icon="ic:outline-explore" />
-      <NavButon icon="fluent:add-circle-24-regular" />
-      <NavButon icon="bxs:user-circle" link="/account/login" />
+    <div className="flex flex-col">
+      { isLoggedIn && width < 768 && showDropdown && <DropdownMenu/> }
+      <div className={className}>
+        <NavButon icon="ant-design:home-filled" />
+        <NavButon icon="ic:outline-explore" />
+        <NavButon icon="fluent:add-circle-24-regular" />
+        
+        { isLoggedIn ? <NavButon icon="bxs:user-circle" onClick={toggleDropdown}/> : <NavButon icon="bxs:user-circle" link="/account/login"/>}
+      </div>
+      { isLoggedIn && width >= 768 && showDropdown && <DropdownMenu/> }
     </div>
   );
 };
