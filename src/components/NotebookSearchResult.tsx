6887a0e9d35d8b807bbe69f51076ba0c6d9ad6f0
Zero1Exp <reyestobias.037@gmail.com>
2022-07-14 00:10:15 -0300

    feat: Add NotebookSearchResult component

---
+++ src/components/NotebookSearchResult.tsx
@@ -0,0 +1,43 @@
+import { INotebookResult } from "../services/GetMockSearchResults";
+import { useState } from "react";
+
+export const NotebookSearchResult = (data: INotebookResult) => {
+  const [joined, setJoined] = useState<boolean>(data.joinedByUser);
+
+  return (
+    <div className="justify-center items-center md:p-0">
+      <div className="flex flex-row justify-evenly">
+        <div className="flex flex-row justify-between w-full mb-4">
+          <div className="flex flex-row items-center">
+            <img className="h-12 w-12 rounded-3xl" src={data.thumbnail} />
+            <div className="ml-2">
+              <h2 className="mr-48 text-primary-800 text-base font-medium">
+                {data.name}
+              </h2>
+              <p className="text-primary-400 text-xs font-extralight line-clamp-2">
+                {data.description}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-row justify-between w-52 ml-5 mb-4">
+          <div>
+            <p className="text-primary-400 text-xs font-extralight text-right">
+              {data.memberCount} Miembros
+            </p>
+            <button
+              onClick={() => setJoined(!joined)}
+              className={
+                joined
+                  ? "bg-primary-400/70 rounded font-semibold text-sm text-white/70 relative inline-block h-7 w-32"
+                  : "bg-primary-400 rounded font-semibold text-sm text-white relative inline-block h-7 w-32"
+              }
+            >
+              <p>{joined ? "Unido" : "Unirse"}</p>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
