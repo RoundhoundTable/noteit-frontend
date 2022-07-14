382ed0e0dd98feffbbb55e5b7922ef3b07302e7b
Zero1Exp <reyestobias.037@gmail.com>
2022-07-14 00:14:45 -0300

    feat: Add UserSearchResult component

---
+++ src/components/UserSearchResult.tsx
@@ -0,0 +1,20 @@
+export interface IUserSearchResult {
+    thumbnail: string;
+    username: string;
+    displayName: string;
+  }
+  
+
+export const UserSearchResult = (props: IUserSearchResult) => {
+    return (
+        <div className="flex flex-row justify-between w-full mb-4">
+            <div className="flex flex-row items-center">
+                <img className="h-12 w-12 rounded-3xl " src={props.thumbnail} alt="User Profile"/>
+                <div className="ml-2">
+                    <h2 className="mr-48 text-primary-800 text-base font-medium">{props.displayName}</h2>
+                    <div className="text-primary-800 font-light text-xs">u/{props.username}</div>
+                </div>
+            </div>
+        </div>
+    );
+};
