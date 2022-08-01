7e24c61773cca2185bec0ba1faa95e30080abad9
Monsterinhouse <lucas.cruz2003@hotmail.com>
2022-08-01 00:11:45 -0300

    feat: Add AddNote page

---
+++ src/components/RichTextEditor.tsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+import { useState } from "react";
+import { EditorState, convertToRaw } from "draft-js";
+import { Editor } from "react-draft-wysiwyg";
+import "./RichText.css";
+import placeholder from "../assets/images/placeholder.jpg";
+import { Icon } from "@iconify/react";
+
+const RichTextEditor = () => {
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+  const onEditorStateChange = (state: any) => {
+    setEditorState(state);
+  };
+
+  return (
+    <Editor
+      editorState={editorState}
+      wrapperClassName="wrapper-class"
+      editorClassName="editor-class"
+      toolbarClassName="toolbar-class"
+      toolbar={{
+        options: ["inline", "list", "textAlign", "remove"],
+        inline: {
+          //  bold: { icon: <Icon icon="carbon:text-bold" /> },
+        },
+      }}
+      onEditorStateChange={onEditorStateChange}
+    />
+  );
+};
+
+export default RichTextEditor;
