55910306e64a9f52b0847710ed93b8e441ff3ef6
Zero1Exp <reyestobias.037@gmail.com>
2022-08-02 19:43:28 -0300

    refactor: Fix Css

--- src/components/SearchbarResultsContainer.tsx
+++ src/components/SearchbarResultsContainer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const SearchbarResultsContainer = (props: React.PropsWithChildren<any>) => {
     return (
       <div className="relative">
-        <ul className="ml-2 z-10 absolute bg-white w-11/12 mt-3 rounded shadow-xl shadow-primary-500/20">
+        <ul className="p-2 ml-2 z-10 absolute bg-white w-11/12 mt-3 rounded shadow-xl shadow-primary-500/20">
             {props.children}
         </ul>
       </div>
