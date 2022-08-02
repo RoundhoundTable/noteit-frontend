19437834329b5403cc53656aafcb0c1010a355d4
Zero1Exp <reyestobias.037@gmail.com>
2022-08-02 19:42:06 -0300

    refactor: Fix Css

--- src/components/SearchResultContainer.tsx
+++ src/components/SearchResultContainer.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export const SearchResultContainer = (props: React.PropsWithChildren<any>) => {
     return (
-        <div className="flex flex-col justify-center items-center my-3 mb-16 p-8 md:p-0">
-            <div className="p-4 max-w-xl shadow-xl  shadow-primary-500/30 flex flex-col pt-5 mt-5 max-h-max px-3">
+        <div className="flex flex-col justify-center items-center p-8 md:p-0">
+            <div className="p-4 max-w-xl shadow-xl shadow-primary-500/30 flex flex-col pt-5 max-h-max">
                 {props.children}
             </div>
         </div>
