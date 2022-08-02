e08ac694efc383c4305a5024640186d0a795c06d
Zero1Exp <reyestobias.037@gmail.com>
2022-08-02 19:41:37 -0300

    refactor: Fix Css

--- src/components/NotebookSearchResult.tsx
+++ src/components/NotebookSearchResult.tsx
@@ -10,26 +10,35 @@ export const NotebookSearchResult = (data: any) => {
           <div className="flex flex-row items-center">
             <img className="h-12 w-12 rounded-3xl" src={data.thumbnail} />
             <div className="ml-2">
-              <h2 className="mr-48 text-primary-800 text-base font-medium">
+              <h2 className="text-primary-800 text-base font-medium">
                 {data.name}
               </h2>
+              <div className="flex md:hidden">
+              <p className="text-primary-400 text-xs font-extralight text-right">
+                {data.memberCount} Miembros
+              </p>
+              </div>
+              <div className="hidden md:flex">
               <p className="text-primary-400 text-xs font-extralight line-clamp-2">
                 {data.description}
               </p>
+              </div>
             </div>
           </div>
         </div>
-        <div className="flex flex-row justify-between w-52 ml-5 mb-4">
+        <div className="flex flex-row justify-around">
           <div>
+            <div className="hidden md:flex">
             <p className="text-primary-400 text-xs font-extralight text-right">
               {data.memberCount} Miembros
             </p>
+            </div>
             <button
               onClick={() => setJoined(!joined)}
               className={
                 joined
-                  ? "bg-primary-400/70 rounded font-semibold text-sm text-white/70 relative inline-block h-7 w-32"
-                  : "bg-primary-400 rounded font-semibold text-sm text-white relative inline-block h-7 w-32"
+                  ? "bg-primary-400/70 rounded font-semibold text-sm text-white/70 relative inline-block w-20 md:h-7 md:w-32"
+                  : "bg-primary-400 rounded font-semibold text-sm text-white relative inline-block w-20 md:h-7 md:w-32"
               }
             >
               <p>{joined ? "Unido" : "Unirse"}</p>
