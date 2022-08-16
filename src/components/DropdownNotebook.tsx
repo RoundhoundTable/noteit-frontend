cb27885a166d0130415beb1fe1fd81ef52dd91f5
Ignun <bajunazu@gmail.com>
2022-08-15 21:25:22 -0300

    Feat: add DropdownNotebook component

---
+++ src/components/DropdownNotebook.tsx
@@ -0,0 +1,19 @@
+import { Icon } from "@iconify/react";
+
+export const DropdownNotebook = ({ role }: {role: string}) => {
+    return (
+        <div className="relative w-full">
+            <ul className="z-10 absolute right-0 bottom-0 md:bottom-auto w-52 bg-white my-3 rounded shadow-xl shadow-primary-500/30 text-center text-primary-500">
+                <li>
+                    <button className="p-2 font-semibold flex flex-row gap-1 justify-center items-center w-full">
+                        { role === "User" &&  <><Icon className="w-5 h-5" icon="ic:baseline-add-moderator"/><p>Mode</p></>}
+                        { role === "Mod" &&  <><Icon className="w-5 h-5" icon="ic:baseline-remove-moderator"/><p>Moden't</p></>}
+                    </button>
+                </li>
+                <li>
+                    <button className="p-2 font-semibold text-danger-500 flex flex-row gap-1 justify-center items-center w-full">Kick</button>
+                </li>
+            </ul>
+        </div>
+    );
+}
