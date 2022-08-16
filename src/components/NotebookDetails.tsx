9124d91609c3d02f87447ba60d0a3ec369a323f0
Ignun <bajunazu@gmail.com>
2022-08-15 21:23:37 -0300

    Feat: add delete constant showModal

--- src/components/NotebookDetails.tsx
+++ src/components/NotebookDetails.tsx
@@ -1,10 +1,17 @@
 import { Icon } from "@iconify/react";
+import { useState } from "react";
+import placeholderPath from "../assets/images/placeholder.jpg";
+import { DropdownNotebook } from "./DropdownNotebook";
+import { UserDisplay } from "./UserDisplay";
 
 interface INotebookDetails {
     isEditMode: boolean;
 }
 
 export const NotebookDetails = ({ isEditMode }: INotebookDetails) => {
+    const [showModal, setShowModal] = useState<boolean>(false);
+    const toggleModal = () => setShowModal(!showModal);
+
     return (
         <div className="bg-primary-600 text-white p-5 h-max md:w-96 md:mt-5">
             <div className="border-b-2 border-primary-800 pb-2">
@@ -20,10 +27,32 @@ export const NotebookDetails = ({ isEditMode }: INotebookDetails) => {
                     )
                 }
             </div>
-            <div className="py-2 max-w-lg flex flex-row items-center gap-2">
+            <button className="py-2 max-w-lg flex flex-row items-center gap-2" onClick={toggleModal}>
                 <Icon className="h-8 w-8 text-white" icon="bxs:user" />
-                Usuarios: 2
-            </div>
+                <p>Usuarios: 2</p>
+            </button>
+            
+                {
+                showModal &&
+                    <div className="text-black w-screen h-screen fixed top-0 left-0 grid items-center justify-items-center z-10 bg-primary-200/10 backdrop-blur-[2px]">
+                        <div className="bg-white w-1/3 rounded border-2 border-primary-500">
+                            <div className="flex flex-col">
+                                <button className="flex flex-row justify-end m-2" onClick={toggleModal}>
+                                    <Icon className="h-6 w-6 text-red-600" icon="akar-icons:circle-x" />
+                                </button>
+                            </div>
+                            <div className="flex flex-col px-8 pb-4">
+                                <p className="text-base text-primary-700 border-b-2 border-primary-700">Lista de miembros:</p>
+                                <ul className="flex flex-col gap-5 p-2">
+                                    <UserDisplay role="Owner"/>
+                                    <UserDisplay role="Mod"/>
+                                    <UserDisplay role="User"/>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                }
+            
         </div>
     )
 }
