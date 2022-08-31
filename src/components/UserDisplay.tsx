7997543632624dbd2c157c3b6cadddf081245b83
Fabricio Futryk <ffutryk52@gmail.com>
2022-08-31 02:45:49 -0300

    feat: Add backend connection

--- src/components/UserDisplay.tsx
+++ src/components/UserDisplay.tsx
@@ -1,29 +1,59 @@
 import { Icon } from "@iconify/react";
-import { useState } from "react";
-import placeholderPath from "../assets/images/placeholder.jpg"
-import { DropdownNotebook } from "./DropdownNotebook";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { ERoles } from "../enums/ERoles";
+import { Notebook, User } from "../interfaces/Entities";
+import { DropdownMenu } from "./DropdownMenu";
+import { UserManagerDropdown } from "./UserManagerDropdown";
 
-export const UserDisplay = ({ role }: {role: string}) => {
-    const [showDropdown, setShowDropdown] = useState<boolean>(false);
-    const toggleDropdown = () => setShowDropdown(!showDropdown);
-
-    return (
-        <li className="flex flex-row justify-between">
-        <div className="flex flex-row items-center gap-2 md:pr-20">
-            <img src={placeholderPath} className="w-10 h-10 rounded-full"></img>
-            <div>
-                <p className="font-semibold break-all">User-Moder</p>
-                <p className="font-light text-sm">u/User-Moder</p>
-            </div>
-            { role === "Owner" && <Icon className="w-5 h-5 text-primary-600" icon="bxs:crown" />}
-            { role === "Mod" && <Icon className="w-5 h-5 text-primary-600" icon="jam:hammer-f" />}
-        </div>
-        <div>
-            <button onClick={toggleDropdown}><Icon className="opcion" icon="simple-line-icons:options-vertical" /></button>
-            {
-                showDropdown && (<DropdownNotebook role={role}/>)
-            }
-        </div>
-    </li>
-    );
+interface UserDisplayProps {
+  user: User;
+  role: ERoles;
+  notebook: Notebook;
 }
+
+export const UserDisplay = ({ user, role, notebook }: UserDisplayProps) => {
+  const [showDropdown, setShowDropdown] = useState<boolean>(false);
+  const dropdownHandler = (event: any) => {
+    setShowDropdown(!showDropdown);
+    event.stopPropagation();
+  };
+
+  return (
+    <li className="flex flex-row justify-between">
+      <Link to={`/u/${user.username}`}>
+        <div className="flex flex-row items-center gap-2">
+          <img src={user.thumbnail} className="w-10 h-10 rounded-full"></img>
+          <div>
+            <p className="font-semibold">{user.displayName}</p>
+            <p className="font-light text-sm">u/{user.username}</p>
+          </div>
+          {role === ERoles.OWNER && (
+            <Icon className="w-5 h-5 text-yellow-600" icon="bxs:crown" />
+          )}
+          {role === ERoles.MODERATOR && (
+            <Icon className="w-5 h-5 text-primary-600" icon="jam:hammer-f" />
+          )}
+        </div>
+      </Link>
+      <div>
+        {(notebook.role === ERoles.OWNER ||
+          (notebook.role === ERoles.MODERATOR && role === ERoles.USER)) &&
+          role !== ERoles.OWNER && (
+            <button onClick={dropdownHandler}>
+              <Icon icon="simple-line-icons:options-vertical" />
+            </button>
+          )}
+        {showDropdown && (
+          <DropdownMenu togglerFunc={setShowDropdown}>
+            <UserManagerDropdown
+              notebook={notebook}
+              user={user}
+              userRole={role}
+            />
+          </DropdownMenu>
+        )}
+      </div>
+    </li>
+  );
+};
