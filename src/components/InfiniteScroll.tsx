36b5855bf26b84e008179c6736ed3ebb42fe871c
Fabricio Futryk <ffutryk52@gmail.com>
2022-08-31 02:27:30 -0300

    feat: Add infinite scroll for notes

---
+++ src/components/InfiniteScroll.tsx
@@ -0,0 +1,26 @@
+import InfiniteScrollComponent from "react-infinite-scroll-component";
+import { Note } from "../interfaces/Entities";
+import { NotePreview } from "./Note/NotePreview";
+
+interface INoteInfiniteScroll {
+  notes: Note[];
+  hasMore: boolean;
+  fetchMore: () => Promise<void>;
+}
+
+export const NoteInfiniteScroll = ({
+  notes,
+  hasMore,
+  fetchMore,
+}: INoteInfiniteScroll) => (
+  <InfiniteScrollComponent
+    dataLength={notes.length}
+    loader={<h4>Loading...</h4>}
+    hasMore={hasMore}
+    next={fetchMore}
+  >
+    {notes.map((note: Note, key: number) => (
+      <NotePreview {...note} key={key} />
+    ))}
+  </InfiniteScrollComponent>
+);
