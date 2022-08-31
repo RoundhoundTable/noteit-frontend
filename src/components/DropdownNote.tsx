94b1138eef7d7b2e69392d1dd020f609c23a8adf
Fabricio Futryk <ffutryk52@gmail.com>
2022-08-31 02:42:57 -0300

    refactor: Remove container + Add link

--- src/components/DropdownNote.tsx
+++ src/components/DropdownNote.tsx
@@ -1,22 +1,27 @@
 import { Icon } from "@iconify/react";
+import { Link } from "react-router-dom";
 
-export const DropdownNote = () => {
-    return (
-        <div className="relative w-full">
-            <ul className="z-10 absolute right-0 bottom-0 md:bottom-auto bg-white mb-3 rounded shadow-xl shadow-primary-500/30 text-center text-primary-500 px-5">
-                <li>
-                    <button className="p-2 font-semibold flex flex-row gap-2 items-center w-full hover:bg-gray-200">
-                        <Icon className="w-5 h-5 text-primary-500" icon="ant-design:edit-filled"></Icon>
-                        <p>Edit</p>
-                    </button>
-                </li>
-                <li>
-                    <button className="p-2 font-semibold text-danger-500 flex flex-row gap-2 items-center w-full hover:bg-gray-200">
-                        <Icon className="w-5 h-5 text-danger-500" icon="bi:trash-fill"/>
-                        <p>Delete</p>
-                    </button>
-                </li>
-            </ul>
-        </div>
-    );
-}
+export const DropdownNoteOptions = ({ noteId }: { noteId: string }) => {
+  return (
+    <>
+      <li>
+        <Link
+          to={`/n/edit/${noteId}`}
+          className="p-2 font-semibold flex flex-row gap-2 items-center w-full hover:bg-gray-200"
+        >
+          <Icon
+            className="w-5 h-5 text-primary-500"
+            icon="ant-design:edit-filled"
+          ></Icon>
+          <p>Edit</p>
+        </Link>
+      </li>
+      <li>
+        <button className="p-2 font-semibold text-danger-500 flex flex-row gap-2 items-center w-full hover:bg-gray-200">
+          <Icon className="w-5 h-5 text-danger-500" icon="bi:trash-fill" />
+          <p>Delete</p>
+        </button>
+      </li>
+    </>
+  );
+};
