bd998b1ea4926fddb6a63e9da7e5e7416cb25af3
Fabricio Futryk <ffutryk52@gmail.com>
2022-08-31 02:43:59 -0300

    feat: Add data props

--- src/components/ProfileHeader.tsx
+++ src/components/ProfileHeader.tsx
@@ -1,15 +1,16 @@
-import { useParams } from "react-router-dom";
-import placeholderPath from "../assets/images/placeholder.jpg"
+import { User } from "../interfaces/Entities";
+import { formatDateAgo } from "../utils/formatDateAgo";
 
-export const ProfileHeader = () => {
-    const { username } = useParams()
-    return (
-        <div className="bg-white flex flex-row gap-5 items-center px-4 sm:px-32 h-28 shadow-lg shadow-primary-700/25 w-full">
-            <img src={placeholderPath} className="w-16 h-16 rounded-full"></img>
-            <div className="text-primary-500">
-                <p className="font-semibold text-2xl ">{username}</p>
-                <p className="font-light text-sm">10 years ago</p>
-            </div>
-        </div>
-    );
-}
+export const ProfileHeader = (user: User) => {
+  return (
+    <div className="bg-white flex flex-row gap-5 items-center px-4 sm:px-32 h-28 shadow-lg shadow-primary-700/25 w-full">
+      <img src={user.thumbnail} className="w-16 h-16 rounded-full"></img>
+      <div className="text-primary-500">
+        <p className="font-semibold text-2xl ">{user.displayName}</p>
+        <p className="font-light text-sm">
+          {formatDateAgo(user.createdOn)} ago
+        </p>
+      </div>
+    </div>
+  );
+};
