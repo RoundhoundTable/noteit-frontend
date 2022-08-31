cbfa7f3f8c173b340716ea67baef2366ab537321
Fabricio Futryk <ffutryk52@gmail.com>
2022-08-31 02:32:27 -0300

    style: Prettier

--- src/layouts/AccountLayout.tsx
+++ src/layouts/AccountLayout.tsx
@@ -2,13 +2,13 @@ import { Icon } from "@iconify/react";
 import { Link, Outlet } from "react-router-dom";
 
 export const AccountLayout = () => {
-    return(
-        <div className="flex flex-col w-full">
-            <div className="p-4 flex flex-row items-center text-primary-500">
-                <Icon className="w-8 h-8" icon="ep:arrow-left-bold"/>
-                <Link to="/">Go to Main Page</Link>
-            </div>
-            <Outlet />
-        </div>
-    );
-}
+  return (
+    <div className="flex flex-col w-full font-montserrat">
+      <div className="p-4 flex flex-row items-center text-primary-500">
+        <Icon className="w-8 h-8" icon="ep:arrow-left-bold" />
+        <Link to="/">Go to Main Page</Link>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
