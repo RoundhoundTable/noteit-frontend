1757a0c82a6e552b14a6d3d728c5565663084117
Fabricio Futryk <ffutryk52@gmail.com>
2022-08-31 02:41:03 -0300

    feat: Add children props

--- src/components/CommentContainer.tsx
+++ src/components/CommentContainer.tsx
@@ -1,15 +1,17 @@
-import { Comment } from "../components/Comment";
 import { CommentForm } from "./CommentForm";
 
-export const CommentList = () => (
-    <div className="flex flex-col items-center w-2/3">
-        <article className="px-6 max-h-5xl shadow-xl shadow-primary-500/30 flex flex-col pt-5 mt-5 w-4/5 gap-4">
-            <CommentForm />
-            <Comment />
-            <Comment />
-            <Comment />
-            <Comment />
-            <Comment />
-        </article>
-    </div>
-)
+interface ICommentContainerProps {
+  noteId: string;
+}
+
+export const CommentContainer = ({
+  children,
+  noteId,
+}: React.PropsWithChildren<ICommentContainerProps>) => (
+  <div className="flex flex-col items-center w-2/3">
+    <section className="px-6 max-h-5xl shadow-xl shadow-primary-500/30 flex flex-col py-5 mt-5 w-4/5 gap-4">
+      <CommentForm noteId={noteId} />
+      {children}
+    </section>
+  </div>
+);
