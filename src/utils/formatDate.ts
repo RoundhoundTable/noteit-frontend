d15b709fca9e3649300840732cd90b1b4bdeb068
Fabricio Futryk <ffutryk52@gmail.com>
2022-08-31 02:03:11 -0300

    fix: Fix displaying wrong date & hour

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -2,11 +2,11 @@ export const formatDate = (dateString: string): string => {
   const date: Date = new Date(dateString);
 
   const day = String(date.getUTCDate()).padStart(2, "0");
-  const month = String(date.getUTCMonth()).padStart(2, "0");
+  const month = String(date.getUTCMonth() + 1).padStart(2, "0");
   const year = date.getUTCFullYear();
 
-  const hours = String(date.getUTCHours()).padStart(2, "0");
-  const minutes = String(date.getUTCMinutes()).padStart(2, "0");
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
 
   return `${day}/${month}/${year} ${hours}:${minutes}`;
 };
