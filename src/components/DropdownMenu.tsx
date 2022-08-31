f18891a5cb9c4ca36429747f21a1a727252ffbf3
Fabricio Futryk <ffutryk52@gmail.com>
2022-08-31 02:41:51 -0300

    refactor: Add children prop to set content

--- src/components/DropdownMenu.tsx
+++ src/components/DropdownMenu.tsx
@@ -1,24 +1,21 @@
-import { Icon } from "@iconify/react";
-import { Link } from "react-router-dom";
+import { MutableRefObject, PropsWithChildren } from "react";
+import { useOutsideClick } from "../hooks/useOutsideClick";
 
-export const DropdownMenu = () => {
-    return (
-        <div className="relative w-full">
-            <ul className="z-10 absolute right-0 bottom-0 md:bottom-auto w-52 bg-white my-3 rounded shadow-xl shadow-primary-500/30 text-center text-primary-500">
-                <Link to="/u/:username">
-                    <li>
-                        <button className="p-2 font-semibold flex flex-row gap-1 justify-center items-center w-full"><Icon className="w-6 h-6" icon="bxs:user-circle"/>Perfil</button>
-                    </li>
-                </Link>
-                <Link to="/account/settings">
-                    <li>
-                        <button className="p-2 font-semibold flex flex-row gap-1 justify-center items-center w-full"><Icon className="w-6 h-6" icon="fa6-solid:gear"/>Configuracion</button>
-                    </li>
-                </Link>
-                <li>
-                <button className="p-2 font-semibold text-danger-500 flex flex-row gap-1 justify-center items-center w-full"><Icon className="w-6 h-6" icon="material-symbols:logout-rounded"/>Cerrar sesión</button>
-                </li>
-            </ul>
-        </div>
-    );
+interface IDropdownMenuProps {
+  togglerFunc: (event: any) => void;
 }
+
+export const DropdownMenu = ({
+  togglerFunc,
+  children,
+}: PropsWithChildren<IDropdownMenuProps>) => {
+  const ref = useOutsideClick(togglerFunc) as MutableRefObject<HTMLDivElement>;
+
+  return (
+    <div ref={ref} className="relative w-full">
+      <ul className="z-10 absolute right-0 bottom-0 md:bottom-auto w-52 bg-white my-3 rounded shadow-xl shadow-primary-500/30 text-center text-primary-500">
+        {children}
+      </ul>
+    </div>
+  );
+};
