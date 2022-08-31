1e3733ea7d16d9d8de46eec21b03167220e3b975
Fabricio Futryk <ffutryk52@gmail.com>
2022-08-31 02:39:47 -0300

    feat: Add props + NoteIt custom button usage

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,13 +1,26 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { BUTTONS, NoteItButton } from "../components/NoteIt/Button";
 
-export const ErrorPage = () => {
-    return (
-        <div className="absolute flex flex-col w-screen h-screen items-center justify-center">
-            <h1 className="font-extrabold text-9xl text-primary-500/70">404</h1>
-            <p className="text-xl mt-4">Lo sentimos, esta página no existe</p>
-            <div className="bg-primary-400 text-white w-64 h-8 flex flex-row items-center justify-center rounded mt-16">
-                <Link to="/" className="font-semibold text-base">Volver al Inicio</Link>
-            </div>
-        </div>
-    )
+interface IErrorPageProps {
+  title: string;
+  message: string;
 }
+
+export const ErrorPage = (props: IErrorPageProps) => {
+  return (
+    <div className="absolute flex flex-col w-screen h-screen items-center justify-center">
+      <h1 className="font-extrabold text-7xl text-primary-500/70">
+        {props.title}
+      </h1>
+      <p className="text-xl mt-4">{props.message}</p>
+      <NoteItButton
+        type={BUTTONS.PRIMARY}
+        className="w-64 flex flex-row items-center justify-center rounded mt-16"
+      >
+        <Link to="/" className="font-semibold text-base w-full">
+          Volver al Inicio
+        </Link>
+      </NoteItButton>
+    </div>
+  );
+};
