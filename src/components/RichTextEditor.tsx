7e4ca58ddb118bf0a571997dbffe5dfd49fde3b0
Fabricio Futryk <ffutryk52@gmail.com>
2022-08-31 02:44:48 -0300

    feat: Add content and error props

--- src/components/RichTextEditor.tsx
+++ src/components/RichTextEditor.tsx
@@ -1,31 +1,57 @@
-import * as React from "react";
 import { useState } from "react";
-import { EditorState, convertToRaw } from "draft-js";
+import { EditorState, convertToRaw, convertFromRaw } from "draft-js";
 import { Editor } from "react-draft-wysiwyg";
-import "../styles/RichText.css";
-import placeholder from "../assets/images/placeholder.jpg";
-import { Icon } from "@iconify/react";
+import "../styles/RichTextEditor.css";
 
-const RichTextEditor = () => {
-  const [editorState, setEditorState] = useState(EditorState.createEmpty());
-  const onEditorStateChange = (state: any) => {
+interface IRichTextEditorProps {
+  cb: (content: string) => void;
+  initialContent?: string;
+  error?: {
+    message?: string;
+  };
+}
+
+const RichTextEditor = ({
+  cb,
+  initialContent,
+  error,
+}: IRichTextEditorProps) => {
+  const [editorState, setEditorState] = useState(
+    initialContent
+      ? EditorState.createWithContent(
+          convertFromRaw(JSON.parse(initialContent))
+        )
+      : EditorState.createEmpty()
+  );
+
+  const onEditorStateChange = (state: EditorState) => {
+    const currentContent = state.getCurrentContent();
+    const raw = convertToRaw(currentContent);
+    const stringifiedContent = JSON.stringify(raw);
+
+    if (!currentContent.hasText()) cb("");
+    else cb(stringifiedContent);
     setEditorState(state);
   };
 
   return (
-    <Editor
-      editorState={editorState}
-      wrapperClassName="wrapper-class"
-      editorClassName="editor-class"
-      toolbarClassName="toolbar-class"
-      toolbar={{
-        options: ["inline", "list", "textAlign", "remove"],
-        inline: {
-          //  bold: { icon: <Icon icon="carbon:text-bold" /> },
-        },
-      }}
-      onEditorStateChange={onEditorStateChange}
-    />
+    <>
+      <Editor
+        editorState={editorState}
+        toolbar={{
+          options: ["inline", "list", "textAlign", "remove"],
+        }}
+        onEditorStateChange={onEditorStateChange}
+        wrapperClassName={`${
+          error?.message ? "border-2 border-danger-500 rounded-lg" : ""
+        }`}
+      />
+      {error?.message && (
+        <div className="text-xs font-light text-danger-500">
+          {error.message}
+        </div>
+      )}
+    </>
   );
 };
 
