5c10cba2b1118b35f95488ead6e069159213835e
Fabricio Futryk <ffutryk52@gmail.com>
2022-08-31 02:26:06 -0300

    feat: Add mobile search page

---
+++ src/pages/MobileSearchPage.tsx
@@ -0,0 +1,7 @@
+import { Searchbar } from "../components/Searchbar";
+
+export const MobileSearchPage = () => (
+  <div className="flex flex-row align-middle justify-center mt-5">
+    <Searchbar />
+  </div>
+);
