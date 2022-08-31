fdeeda5c4c9fc98dc2062d29770584dfd5bf802b
Fabricio Futryk <ffutryk52@gmail.com>
2022-08-31 02:34:40 -0300

    feat: Add comment infinite scroll + backend connection

--- src/pages/NotePage.tsx
+++ src/pages/NotePage.tsx
@@ -1,9 +1,37 @@
-import { FullNote } from "../components/FullNote";
-import { CommentList } from "../components/CommentContainer";
+import { useApolloClient, useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
+import { NoteView } from "../components/Note/NoteView";
+import { CommentInfiniteScroll } from "../components/CommentInfiniteScroll";
+import { useEffect } from "react";
+import { CommentContainer } from "../components/CommentContainer";
+import { note, noteData, noteVars } from "../graphql/queries/note";
+import { commentsVars } from "../graphql/queries/comments";
 
-export const NotePage = () => (
+export const NotePage = () => {
+  const apolloClient = useApolloClient();
+  const { id } = useParams();
+  const { data } = useQuery<noteData, noteVars>(note, {
+    variables: {
+      noteId: id!,
+    },
+  });
+
+  const queryVariables: commentsVars = {
+    noteId: id!,
+    limit: 5,
+    offset: 0,
+  };
+
+  useEffect(() => {
+    apolloClient.cache.reset();
+  }, []);
+
+  return (
     <div className="flex flex-col items-center">
-        <FullNote />
-        <CommentList />
+      {data && <NoteView {...data.note} />}
+      <CommentContainer noteId={id!}>
+        <CommentInfiniteScroll variables={queryVariables} />
+      </CommentContainer>
     </div>
-)
+  );
+};
