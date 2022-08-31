2f2e83976db92bb446c68918a583f8dbfeacfcb3
Fabricio Futryk <ffutryk52@gmail.com>
2022-08-31 02:08:35 -0300

    feat: Add mutation types enum

---
+++ src/enums/EMutationTypes.ts
@@ -0,0 +1,27 @@
+export enum ENotebookMutationType {
+  CREATE = "CREATE",
+  EDIT = "EDIT",
+  DELETE = "DELETE",
+}
+
+export enum ENoteMutationType {
+  CREATE = "CREATE",
+  EDIT = "EDIT",
+  DELETE = "DELETE",
+}
+
+export enum EMembershipMutationType {
+  JOIN = "JOIN",
+  LEAVE = "LEAVE",
+  KICK = "KICK",
+  EDIT = "EDIT",
+}
+
+export enum ECommentMutationType {
+  CREATE = "CREATE",
+  DELETE = "DELETE",
+}
+
+export enum EUserMutationType {
+  EDIT = "EDIT",
+}
