6f262833886ac87f0f816cca8f9a16715cf7797c
Fabricio Futryk <ffutryk52@gmail.com>
2022-08-31 02:43:33 -0300

    feat: Add backend connection

--- src/components/NotebookHeader.tsx
+++ src/components/NotebookHeader.tsx
@@ -1,44 +1,114 @@
-import { useState } from "react";
-import { useParams } from "react-router-dom";
-import placeholderPath from "../assets/images/placeholder.jpg"
+import { useEffect, useState } from "react";
 import { Icon } from "@iconify/react";
-import { useField } from "../hooks/useField";
+import { ERoles } from "../enums/ERoles";
+import { formatDateAgo } from "../utils/formatDateAgo";
+import { useMutation } from "@apollo/client";
+import { membership } from "../graphql/mutations/membership";
+import { Notebook } from "../interfaces/Entities";
+import {
+  EMembershipMutationType,
+  ENotebookMutationType,
+} from "../enums/EMutationTypes";
+import { ImageInput } from "./ImageInput";
+import { notebook } from "../graphql/mutations/notebook";
+import { BUTTONS, NoteItButton } from "./NoteIt/Button";
 
 interface INotebookHeaderProps {
-    isEditMode: boolean;
-    editModeToggler: () => void;
+  data: Notebook;
+  isEditMode: boolean;
+  submitChanges: () => void;
+  setImageData: (fileData: any) => void;
+  editModeToggler: () => void;
 }
 
-export const NotebookHeader = ({ isEditMode, editModeToggler }: INotebookHeaderProps) => {
-    const name = useField({ type: "text"})
-    const { notebookid } = useParams()
-    const [ joined, setJoined ] = useState<boolean>(false)
-    return (
-        <div className="bg-white flex flex-col lg:flex-row justify-between items-center p-4 md:px-32 shadow-lg shadow-primary-700/25 w-full gap-5">
-            <div className="flex flex-row gap-2 md:gap-10">
-                <img src={placeholderPath} className="w-16 h-16 rounded-full"></img>
-                <div className="text-primary-500">
-                    { isEditMode ? <input {...name} className="font-semibold text-primary-500 px-3 h-11 focus:outline-none rounded border-2 border-primary-500"/>:<p className="font-semibold text-2xl ">{ notebookid }</p>}
-                    <p className="font-light text-sm">20 years ago</p>
-                </div>
-            </div>
-            <div className="flex flex-row gap-5">
-                <button onClick={() => setJoined (!joined)} className={
-                    joined
-                    ? "bg-primary-500 p-1 h-10 border-2 border-blue-400 hover:bg-primary-600 rounded text-white"
-                    : "bg-primary p-1 h-10 border-2 border-danger-400 hover:bg-danger-600 hover:text-white rounded text-red-600"
-                }>
-                    {joined ? "Unirse" : "Abandonar"}
-                </button>
-                {isEditMode? 
-                    <><button className="bg-primary-500 px-4 h-10 border-2 border-blue-400 hover:bg-primary-600 rounded text-white">Guardar</button>
-                    <button onClick={editModeToggler} className="bg-primary p-1 h-10 border-2 border-danger-400 hover:bg-danger-600 hover:text-white rounded text-red-600">Cancelar</button></>:
+export const NotebookHeader = ({
+  data,
+  isEditMode,
+  editModeToggler,
+  setImageData,
+  submitChanges,
+}: INotebookHeaderProps) => {
+  const [EditMembership, { data: _data }] = useMutation(membership);
+  const [joined, setJoined] = useState<boolean>(data.joinedByUser!);
 
-                    <button onClick={editModeToggler}>
-                        <Icon className="w-10 h-10 text-primary-500" icon="ant-design:edit-filled"></Icon>
-                    </button>
-                }
-            </div>
+  const handleMembership = async () => {
+    await EditMembership({
+      variables: {
+        type: joined
+          ? EMembershipMutationType.LEAVE
+          : EMembershipMutationType.JOIN,
+        payload: {
+          notebookName: data.name,
+        },
+      },
+    });
+  };
+
+  useEffect(() => {
+    if (_data?.membership.left) {
+      setJoined(false);
+    }
+    if (_data?.membership.joined) {
+      setJoined(true);
+    }
+  }, [_data]);
+
+  return (
+    <div className="bg-white flex flex-col lg:flex-row justify-between items-center p-4 md:px-32 shadow-lg shadow-primary-700/25 w-full gap-5">
+      <div className="flex flex-row gap-2 md:gap-10">
+        {isEditMode ? (
+          <ImageInput
+            className="w-16 h-16"
+            displayImage={data.thumbnail!}
+            cb={setImageData}
+          />
+        ) : (
+          <img src={data.thumbnail} className="w-16 h-16 rounded-full"></img>
+        )}
+        <div className="text-primary-500">
+          <p className="font-semibold text-2xl ">{data.name}</p>
+          <p className="font-light text-sm">
+            {formatDateAgo(data.createdOn!)} ago
+          </p>
         </div>
-    )
-}
+      </div>
+      <div className="flex flex-row gap-5">
+        {!(data.role === ERoles.OWNER) && (
+          <NoteItButton
+            type={joined ? BUTTONS.DANGER : BUTTONS.PRIMARY}
+            onClick={handleMembership}
+            className={"w-28"}
+          >
+            {joined ? "Abandonar" : "Unirse"}
+          </NoteItButton>
+        )}
+        {data.role === ERoles.OWNER &&
+          (isEditMode ? (
+            <>
+              <NoteItButton
+                type={BUTTONS.PRIMARY}
+                onClick={submitChanges}
+                className={"w-28"}
+              >
+                Guardar
+              </NoteItButton>
+              <NoteItButton
+                type={BUTTONS.DANGER}
+                onClick={editModeToggler}
+                className={"w-28"}
+              >
+                Cancelar
+              </NoteItButton>
+            </>
+          ) : (
+            <button onClick={editModeToggler}>
+              <Icon
+                className="w-10 h-10 text-primary-500"
+                icon="ant-design:edit-filled"
+              ></Icon>
+            </button>
+          ))}
+      </div>
+    </div>
+  );
+};
