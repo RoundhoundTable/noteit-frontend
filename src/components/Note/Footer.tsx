bb4b82de07d4b2e57a32a64cb6ee261af9122194
Ignun <bajunazu@gmail.com>
2022-10-02 20:32:02 -0300

    Refactor: fix css

--- src/components/Note/Footer.tsx
+++ src/components/Note/Footer.tsx
@@ -2,6 +2,6 @@ import { PropsWithChildren } from "react";
 
 export const Footer = (props: PropsWithChildren<any>) => {
   return (
-    <div className="flex flex-row justify-evenly mt-4 ">{props.children}</div>
+    <div className="flex flex-row justify-evenly mt-4">{props.children}</div>
   );
 };
