b14d272338f43597c43c16a1bb3a977bf6028325
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-17 20:04:31 -0300

    feat: Add DeleteNote functionality

--- src/components/DropdownNote.tsx
+++ src/components/DropdownNote.tsx
@@ -1,7 +1,54 @@
+import { useMutation } from "@apollo/client";
 import { Icon } from "@iconify/react";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { ENoteMutationType } from "../enums/EMutationTypes";
+import { note } from "../graphql/mutations/note";
+import { IModalProps } from "../interfaces/IModalProps";
+import { Modal } from "./Modal";
 
 export const DropdownNoteOptions = ({ noteId }: { noteId: string }) => {
+  const navigateTo = useNavigate();
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [deleteNote, { data }] = useMutation(note);
+
+  console.log({
+    variables: {
+      type: ENoteMutationType.DELETE,
+      payload: {
+        id: noteId,
+      },
+    },
+  });
+
+  const toggleModal = () => setShowModal(!showModal);
+  const deleteNoteModal: IModalProps = {
+    message: "¿Estas seguro de que deseas eliminar la publicacion?",
+    acceptOption: {
+      text: "Aceptar",
+      onClick: () =>
+        deleteNote({
+          variables: {
+            type: ENoteMutationType.DELETE,
+            payload: {
+              id: noteId,
+            },
+          },
+        }),
+    },
+    cancelOption: {
+      text: "Cancelar",
+      onClick: toggleModal,
+    },
+  };
+
+  useEffect(() => {
+    if (data?.comment?.deleted) {
+      toggleModal();
+      navigateTo("/");
+    }
+  }, [data]);
+
   return (
     <>
       <li>
@@ -17,11 +64,15 @@ export const DropdownNoteOptions = ({ noteId }: { noteId: string }) => {
         </Link>
       </li>
       <li>
-        <button className="p-2 font-semibold text-danger-500 flex flex-row gap-2 items-center w-full hover:bg-gray-200">
+        <button
+          className="p-2 font-semibold text-danger-500 flex flex-row gap-2 items-center w-full hover:bg-gray-200"
+          onClick={toggleModal}
+        >
           <Icon className="w-5 h-5 text-danger-500" icon="bi:trash-fill" />
           <p>Delete</p>
         </button>
       </li>
+      {showModal && <Modal {...deleteNoteModal} />}
     </>
   );
 };
